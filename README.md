37ceecd5e8bbdc9e8508c5634bdb0924f5d0ceff
Rhaynel Parra Aguiar <mail@rhynl.io>
2019-08-29 21:47:17 -0300

    refact: accept arguments

--- README.md
+++ README.md
@@ -2,4 +2,16 @@
 
 > Simple static files server
 
-This is a work in progress. For educational purpose only.
+This program can serve static files from a given directory at a given port.
+
+## Params
+
+* port: the port where de server will be listening. Default 3001
+
+* path: the path to de directory to be serve. default, current directory.
+
+### Example of use
+
+```sh
+http-server -port 8080 -path example
+```
--- http-server.go
+++ http-server.go
@@ -1,12 +1,25 @@
 package main
 
 import (
+	"flag"
+	"fmt"
 	"net/http"
 	"os"
 )
 
 func main() {
-	dir, _ := os.Getwd()
+	var dir string
 
-	http.ListenAndServe(":8080", http.FileServer(http.Dir(dir)))
+	port := flag.String("port", "3001", "port in which the server will be listen on.")
+	path := flag.String("path", "", "directory to be serve")
+
+	flag.Parse()
+
+	if *path == "" {
+		dir, _ = os.Getwd()
+	} else {
+		dir = *path
+	}
+
+	http.ListenAndServe(fmt.Sprintf(":%s", *port), http.FileServer(http.Dir(dir)))
 }
